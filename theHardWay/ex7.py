6740db6f5b8bec4a632e14768a134a13fe5db637
Matthew Walker <mattwalker101@hotmail.com>
2015-11-06 11:38:52 -0800

    Create ex7.py

---
+++ theHardWay/ex7.py
@@ -0,0 +1,26 @@
+# exercise:
+# Learn Python the Hard Way
+# Matthew Walker
+# Nov 3 2015
+
+print "Mary had a little lamb." # prints the string
+print "Its fleece was white as %s." % 'snow' # prints next line with 'snow'
+print "And everywhere that Mary went." # another line from the rhyme
+print "." * 10 # prints 10 periods in a row
+
+# now a bunch of variables to spell a word or two
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6, #prints the letters with a space instead of a newline
+print end7 + end8 + end9 + end10 + end11 + end12 #and then the next word
