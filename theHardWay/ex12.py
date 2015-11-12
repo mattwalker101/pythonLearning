4892316a3e168d1dcb32c75c4ad397d76cb3a529
Matthew Walker <mattwalker101@hotmail.com>
2015-11-12 10:42:39 -0800

    Create ex12.py

---
+++ theHardWay/ex12.py
@@ -0,0 +1,10 @@
+# exercise: 12
+# Learn Python the Hard Way
+# Matthew Walker
+# Nov 16 2015
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "So, you're %r old, %s tall and %s heavy." % (age, height, weight)
