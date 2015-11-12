b9764941e76116d102de7ce9f43787b6bdde8fcc
Matthew Walker <mattwalker101@hotmail.com>
2015-11-12 10:11:35 -0800

    Create ex10.py

---
+++ theHardWay/ex10.py
@@ -0,0 +1,26 @@
+# exercise: 10
+# Learn Python the Hard Way
+# Matthew Walker
+# Nov 12 2015
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#while True:
+#	for i in ["/","-","|","\\","|"]:
+#		print "%s\r" % i,
+# The above makes a spinning icon thingy.
+		
