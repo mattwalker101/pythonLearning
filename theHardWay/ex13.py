c7b989c4795207cf6b4b5b0543f2f917e08629b9
Matthew Walker <mattwalker101@hotmail.com>
2015-11-12 10:48:38 -0800

    Create ex13.py

---
+++ theHardWay/ex13.py
@@ -0,0 +1,13 @@
+# exercise: 13
+# Learn Python the Hard Way
+# Matthew Walker
+# Nov 16 2015
+
+from sys import argv  # argv is called argument variable
+
+script, first, second, third = argv  #this unpacks argument variable into the 4 variables listed
+
+print "The script is called:", script
+print "The first variable is called:", first
+print "The second variable is called:", second
+print "The third variable is called:", third
